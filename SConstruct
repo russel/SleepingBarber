dbce63efdc2cad0977571fd46b346e37d38483e4
Russel Winder <russel@russel.org.uk>
2010-11-06 19:45:18 +0000

    Add a D2 version.

--- SConstruct
+++ SConstruct
@@ -1,8 +1,21 @@
-import os.path
+import os
 
 goEnvironment = Environment ( tools = [ 'go' ] )
 
 for item in Glob ( '*.go' ) :
     goEnvironment.GoProgram ( os.path.splitext ( item.name ) [0] ,  item )
+
+#  The dmd tool fails to set up the environment correctly to do linking on Ubuntu unless there is a compiler
+#  tool specified in order to determine the linker AND the dmd tool is included after the link and compiler
+#  tools. Also the dmd compiler is not in the bootstrap path.
+
+dEnvironment = Environment (
+    tools = [ 'gcc' , 'gnulink' , 'dmd' ] ,# NB dmd must follow gcc and gnulink.
+    ENV = os.environ ,# dmd is not in the standard place.
+    DFLAGS = [ '-O' , '-release' , '-inline' ] ,
+   )
+
+for item in Glob ( '*.d' ) :
+    dEnvironment.Program ( item )
 
 Clean ( '.' , [ 'scons-go-helper' ] )
---
+++ singleBarber_d2.d
@@ -0,0 +1,95 @@
+//  This is a model of the "The Sleeping Barber" problem using D (http://d-programming-language.org/),
+ //  cf. http://en.wikipedia.org/wiki/Sleeping_barber_problem.
+//
+//  Copyright © 2010 Russel Winder
+
+//  D appears to implement Actor Model when creating spawned processes, i.e. each process has a single
+//  message queue on which receive or receiveOnly can be called. Customers are passed as message between the
+//  processes.  However as each process has only a single message queu we have to realize case classes.
+
+import std.concurrency ;
+import std.random ;
+import std.stdio ;
+
+import core.thread ;
+
+struct Customer {
+  int id ;
+  this ( int id ) { this.id = id ; }
+}
+
+struct SuccessfulCustomer {
+  Customer customer ;
+  this ( Customer customer ) { this.customer = customer ; }
+}
+
+void barber ( long function ( ) hairTrimTime ) {
+  Tid shop ;
+  auto running = true ;
+  while ( running ) {
+    receive (
+             ( Tid tid ) { shop = tid ; } ,
+             ( Customer customer ) {
+               writeln ( "Barber : Starting Customer " , customer.id ) ;
+               Thread.sleep ( hairTrimTime ( ) ) ;
+               writeln ( "Barber : Finished Customer " , customer.id ) ;
+               shop.send ( SuccessfulCustomer ( customer ) ) ;
+             } ,
+             ( OwnerTerminated ) { running = false ; }
+             ) ;
+  }
+}
+
+void shop ( int numberOfSeats , Tid world , Tid barber ) {
+  auto shopClosing = false ;
+  auto seatsFilled = 0 ;
+  auto customersRejected = 0 ;
+  auto customersTrimmed = 0 ;
+  auto running = true ;
+  barber.send ( thisTid ) ;
+  while ( running ) {
+    receive (
+             ( Customer customer ) {
+               if ( seatsFilled < numberOfSeats ) {
+                 seatsFilled++ ;
+                 writeln ( "Shop : Customer " , customer.id , " takes a seat. " , seatsFilled , " in use." ) ;
+                 barber.send ( customer ) ;
+               }
+               else {
+                 customersRejected++ ;
+                 writeln ( "Shop : Customer " , customer.id , " turned away." ) ;
+               }
+             } ,
+             ( SuccessfulCustomer customer ) {
+               customersTrimmed++ ;
+               seatsFilled-- ;
+               writeln ( "Shop : Customer " , customer.customer.id , " leaving trimmed." ) ;
+               if ( shopClosing && ( seatsFilled == 0 ) ) {
+                 writeln ( "\nTrimmed " , customersTrimmed , " customers and rejected " , customersRejected , " today.\n" ) ;
+                 world.send ( "" ) ;
+               }
+             } ,
+             ( string s ) { shopClosing = true ; } ,
+             (OwnerTerminated ) { running = false ; }
+             ) ;
+  }
+}
+
+void world ( int numberOfCustomers , int numberOfSeats , long function ( )  nextCustomerWaitTime , long function ( ) hairTrimTime ) {
+  auto barber = spawn ( & barber , hairTrimTime ) ;
+  auto shop = spawn ( & shop , numberOfSeats , thisTid , barber ) ;
+  for ( auto i = 0 ; i < numberOfCustomers ; ++i ) {
+    Thread.sleep ( nextCustomerWaitTime ( ) ) ;
+    writeln ( "World : Customer " , i , " enters the shop." ) ;
+    shop.send ( Customer ( i ) ) ;
+  }
+  shop.send ( "" ) ;
+  receiveOnly ! ( string ) ( ) ;
+}
+
+void main ( string[] args ) {
+  world ( 20 , 4 ,
+          function long ( ) { return ( cast ( long ) uniform ( 0.0 , 1.0 ) * 200000 ) + 100000 ; } ,
+          function long ( ) { return ( cast ( long ) uniform ( 0.0 , 1.0 ) * 800000 ) + 100000 ; }
+          ) ;
+}
