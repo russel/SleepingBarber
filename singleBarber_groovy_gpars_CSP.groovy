fd52eafa74565fc6a930f220e4c35ac005cdbe69
Russel Winder <russel@russel.org.uk>
2010-11-28 17:20:35 +0000

    Use alting instead of multiple processes, and remove the shared state.

--- singleBarber_groovy_gpars_CSP.groovy
+++ singleBarber_groovy_gpars_CSP.groovy
@@ -18,6 +18,7 @@
 @Grab ( group = 'org.codehaus.jcsp' , module = 'jcsp' , version = '1.1-rc5' )
 @Grab ( group = 'org.codehaus.gpars' , module = 'gpars' , version = '0.11-beta-1' )
 
+import org.jcsp.util.Buffer
 import org.jcsp.lang.Channel
 import org.jcsp.lang.CSProcess
 
@@ -30,15 +31,16 @@ class Customer {
 }
 
 def runSimulation ( int numberOfCustomers , int numberOfWaitingSeats , Closure hairTrimTime , Closure nextCustomerWaitTime ) {
-  final worldToShopInChannel = Channel.one2one ( )
-  final shopInToBarberChannel = Channel.one2one ( )
-  final barberToShopOutChannel = Channel.one2one ( )
+  final worldToShopChannel = Channel.one2one ( )
+  final shopToBarberChannel = Channel.one2one ( new Buffer ( numberOfWaitingSeats ) )
+  final barberToShopChannel = Channel.one2one ( )
   final barber = new CSProcess ( ) {
-    public void run ( ) {
-      def inChannel = shopInToBarberChannel.in ( )
-      def outChannel = barberToShopOutChannel.out ( )
+    @Override public void run ( ) {
+      final inChannel = shopToBarberChannel.in ( )
+      final outChannel = barberToShopChannel.out ( )
       while ( true ) {
-        def customer = inChannel.read ( )
+        final customer = inChannel.read ( )
+        if ( customer == '' ) { break }
         assert customer instanceof Customer
         println ( 'Barber : Starting Customer ' + customer.id )
         Thread.sleep ( hairTrimTime ( ) )
@@ -47,51 +49,56 @@ def runSimulation ( int numberOfCustomers , int numberOfWaitingSeats , Closure h
       }
     }
   }
-  def seatsTaken = 0
-  def customersTurnedAway = 0
-  def customersTrimmed = 0
-  def isOpen = true
-  final barbersShopIn = new CSProcess ( ) {
-    public void run ( ) {
-      def inChannel = worldToShopInChannel.in ( )
-      def outChannel = shopInToBarberChannel.out ( )
+  final barbersShop = new CSProcess ( ) {
+    @Override public void run ( ) {
+      final fromBarberChannel = barberToShopChannel.in ( )
+      final fromWorldChannel = worldToShopChannel.in ( )
+      final outChannel = shopToBarberChannel.out ( )
+      final selector = new ALT ( [ fromBarberChannel , fromWorldChannel ] )
+      def seatsTaken = 0
+      def customersTurnedAway = 0
+      def customersTrimmed = 0
+      def isOpen = true
+mainloop: 
       while ( true ) {
-        def message = inChannel.read ( )
-        if ( message == '' ) { isOpen = false }
-        else {
-          assert message instanceof Customer
-          if ( seatsTaken < numberOfWaitingSeats ) {
-            ++seatsTaken
-            println ( 'Shop : Customer ' + message.id + ' takes a seat. ' + seatsTaken + ' in use.' )
-            outChannel.write ( message )
-          }
-          else {
-            println ( 'Shop : Customer ' + message.id + ' turned away.' )
-            ++customersTurnedAway
-          }
-        }
-      }
-    }
-  }
-  final barbersShopOut = new CSProcess ( ) {
-    public void run ( ) {
-      def inChannel = barberToShopOutChannel.in ( )
-      while ( true ) {
-        def message = inChannel.read ( )
-        assert message instanceof Customer
-        --seatsTaken
-        ++customersTrimmed
-        println ( 'Shop : Customer ' + message.id + ' leaving trimmed.' )
-        if ( ! isOpen && ( seatsTaken == 0 ) ) {
-          println ( '\nTrimmed ' + customersTrimmed + ' and turned away ' + customersTurnedAway + ' today.' )
-          stop ( )
+        switch ( selector.select ( ) ) { 
+         case 0 : // From the Barber
+           def customer = fromBarberChannel.read ( )
+           assert customer instanceof Customer
+           --seatsTaken
+           ++customersTrimmed
+           println ( 'Shop : Customer ' + customer.id + ' leaving trimmed.' )
+           if ( ! isOpen && ( seatsTaken == 0 ) ) {
+             println ( '\nTrimmed ' + customersTrimmed + ' and turned away ' + customersTurnedAway + ' today.' )
+             outChannel.write ( '' )
+             break mainloop
+           }
+           break
+         case 1 : // From the World
+           def customer = fromWorldChannel.read ( )
+           if ( customer == '' ) { isOpen = false }
+           else {
+             assert customer instanceof Customer
+             if ( seatsTaken < numberOfWaitingSeats ) {
+               ++seatsTaken
+               println ( 'Shop : Customer ' + customer.id + ' takes a seat. ' + seatsTaken + ' in use.' )
+               outChannel.write ( customer )
+             }
+             else {
+               println ( 'Shop : Customer ' + customer.id + ' turned away.' )
+               ++customersTurnedAway
+             }
+           }
+           break
+         default :
+           throw new RuntimeException ( 'Shop : Seleced an non-existant channel.' )
         }
       }
     }
   }
   final world = new CSProcess ( ) {
-    public void run ( ) {
-      def toShopChannel = worldToShopInChannel.out ( )
+    @Override public void run ( ) {
+      def toShopChannel = worldToShopChannel.out ( )
       for ( number in 0 ..< numberOfCustomers ) {
         Thread.sleep ( nextCustomerWaitTime ( ) )
         println ( 'World : Customer ' + number + ' enters the shop.' )
@@ -100,7 +107,7 @@ def runSimulation ( int numberOfCustomers , int numberOfWaitingSeats , Closure h
       toShopChannel.write ( '' )
     }
   }
-  new PAR ( [ barber , barbersShopIn , barbersShopOut , world ] ).run ( )
+  new PAR ( [ barber , barbersShop , world ] ).run ( )
 }
 
 runSimulation ( 20 , 4 , { ( Math.random ( ) * 60 + 10 ) as int }, { ( Math.random ( ) * 20 + 10 ) as int } )
