460123ec6eb00798d14c62c905fc73aaf2cf479e
Russel Winder <russel@russel.org.uk>
2011-09-06 03:44:13 +0100

    Get the Java and Groovy pure threads versions to not block.

--- SingleBarber_Java_Threads.java
+++ SingleBarber_Java_Threads.java
@@ -21,59 +21,100 @@ public class SingleBarber_Java_Threads {
     }
     public int call ( ) { return (int)( Math.random ( ) * scale ) + offset ; }
   }
-  private final static class Barber implements Runnable {
-    private int customersTrimmed = 0 ;
+  private ArrayBlockingQueue<Customer> waitingChairs ;
+  private ArrayBlockingQueue<Customer> toShop ;
+  private ArrayBlockingQueue<Customer> fromChair ;
+  private ArrayBlockingQueue<Customer> fromShop ;
+  private int customersTurnedAway ;
+  private int customersTrimmed ;
+  //  Must subclass thread here rather than create a Runnable, so that we can execute an interrupt when
+  //  closing the shop.
+  private final class Barber extends Thread {
     private boolean working = true ;
-    private final ArrayBlockingQueue<Customer> waitingChairs ;
-    final RandomCallingFunction hairTrimTime ;
-    public Barber( final ArrayBlockingQueue<Customer> waitingChairs , final RandomCallingFunction hairTrimTime ) {
-      this.waitingChairs = waitingChairs ;
-      this.hairTrimTime = hairTrimTime ;
+    private final RandomCallingFunction hairTrimTime ;
+    public Barber( final RandomCallingFunction hairTrimTime ) { this.hairTrimTime = hairTrimTime ; }
+    public void stopWork ( ) {
+      working = false ;
+      interrupt ( ) ;
     }
-    public void stopWork ( ) { working = false ; }
     @Override public void run ( ) {
       while ( working || ( waitingChairs.size ( ) > 0 ) ) {
         try {
+          //  Use take here to simulate barber sleeping if there are no customers by blocking.  It means we
+          //  have to use an interrupt when the shop gets closed.
           final Customer customer = waitingChairs.take ( ) ;
           System.out.println ( "Barber : Starting Customer " + customer.id + "." ) ;
           try { Thread.sleep ( hairTrimTime.call ( ) ) ; }
-          catch ( InterruptedException iiee ) { /* Intentionally left blank. */ }
+          catch ( InterruptedException iiee ) { throw new RuntimeException ( iiee ) ; }
           System.out.println ( "Barber : Finished Customer " + customer.id + "." ) ;
+          try { fromChair.put ( customer ) ; }
+          catch ( InterruptedException iiee ) { throw new RuntimeException ( iiee ) ; }
+        }
+        catch ( InterruptedException ie ) { /* Intentionally left blank */ }
+      }
+      System.out.println ( "Barber : Work over for the day." ) ;
+    }
+  }
+  private final class Shop implements Runnable {
+    private boolean open = true ;
+    public void closeShop ( ) { open = false ; }
+    @Override public void run ( ) {
+      while ( open || waitingChairs.size ( ) > 0 ) {
+        Customer customer = toShop.poll ( ) ;
+        if ( customer != null ) {
+          if ( waitingChairs.offer ( customer ) ) {
+            System.out.println ( "Shop : Customer " + customer.id + " takes a seat. " + waitingChairs.size ( ) + " in use." ) ;
+          }
+          else {
+            ++customersTurnedAway ;
+            System.out.println ( "Shop : Customer " + customer.id + " turned away." ) ;
+            try { fromShop.put ( customer ) ; }
+            catch ( InterruptedException ie ) {  throw new RuntimeException ( ie ) ; }
+          }
+        }
+        customer = fromChair.poll ( ) ;
+        if ( customer != null ) {
           ++customersTrimmed ;
           System.out.println ( "Shop : Customer " + customer.id + " leaving trimmed." ) ;
+          try { fromShop.put ( customer ) ; }
+          catch ( InterruptedException ie ) {  throw new RuntimeException ( ie ) ; }
         }
-        catch ( InterruptedException ie ) { /* Intentionally left blank. */ }
       }
+      System.out.println ( "Shop : Closing." ) ;
     }
-    public int getCustomersTrimmed ( ) { return customersTrimmed ; }
-  } 
-  private void runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats ,
-                    final RandomCallingFunction hairTrimTime , final RandomCallingFunction nextCustomerWaitTime ) {
-    final ArrayBlockingQueue<Customer> waitingChairs = new ArrayBlockingQueue<Customer> ( numberOfWaitingSeats ) ;
-    int customersTurnedAway = 0 ;
-    int customersTrimmed = 0 ;
-    final Barber barber = new Barber ( waitingChairs , hairTrimTime ) ;
-    final Thread barberThread = new Thread ( barber ) ;
-    barberThread.start ( ) ;
-    for ( int number = 0 ; number < numberOfCustomers ; ++number ) {
-      try { Thread.sleep ( nextCustomerWaitTime.call ( ) ) ; }
-      catch ( InterruptedException iiee ) { /* Intentionally left blank. */ }     
-      System.out.println ( "World : Customer " + number + " enters the shop." ) ;
-      final Customer customer = new Customer ( number ) ;
-      if ( waitingChairs.offer ( customer ) ) {
-        System.out.println ( "Shop : Customer " + customer.id + " takes a seat. " + waitingChairs.size ( ) + " in use." ) ;
-      }
-      else {
-        ++customersTurnedAway ;
-        System.out.println ( "Shop : Customer " + customer.id + " turned away." ) ;
-      }
-    }
-    barber.stopWork ( ) ;
-    try { barberThread.join ( ) ; }
-    catch ( InterruptedException iiee ) { /* Intentionally left blank. */ }     
-    System.out.println ( "\nTrimmed " + barber.getCustomersTrimmed ( ) + " and turned away " + customersTurnedAway + " today." ) ;
   }
-  public static void main ( final String[] args ) {
+  private void runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats ,
+                    final RandomCallingFunction hairTrimTime , final RandomCallingFunction nextCustomerWaitTime ) throws InterruptedException {
+    waitingChairs = new ArrayBlockingQueue<Customer> ( numberOfWaitingSeats ) ;
+    toShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
+    fromChair = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
+    fromShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
+    customersTurnedAway = 0 ;
+    customersTrimmed = 0 ;
+    final Barber barber = new Barber ( hairTrimTime ) ;
+    barber.start ( ) ;
+    final Shop shop = new Shop ( ) ;
+    final Thread shopThread = new Thread (shop ) ;
+    shopThread.start ( ) ;
+    for ( int number = 0 ; number < numberOfCustomers ; ++number ) {
+      Thread.sleep ( nextCustomerWaitTime.call ( ) ) ;
+      System.out.println ( "World : Customer " + number + " enters the shop." ) ;
+      toShop.put ( new Customer ( number ) ) ;
+    }
+    for ( int number = 0 ; number < numberOfCustomers ; ++number ) {
+      final Customer customer = fromShop.take ( ) ;
+      System.out.println ( "World : Customer " + customer.id + " exits the shop." ) ;
+    }
+    System.out.println ( "World : Time to close up." ) ;
+    //  If we don't get here then Sweeney Todd is the barber — we have not got as many live customers back
+    //  as we put in.
+    barber.stopWork ( ) ;
+    barber.join ( ) ;
+    shop.closeShop ( ) ;
+    shopThread.join ( ) ;
+    System.out.println ( "\nTrimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " today." ) ;
+  }
+  public static void main ( final String[] args ) throws InterruptedException {
     ( new SingleBarber_Java_Threads ( ) ).runSimulation ( 20 , 4 ,
                                                           new RandomCallingFunction ( 60 , 10 ) ,
                                                           new RandomCallingFunction ( 20 , 10 ) ) ;
--- singleBarber_groovy_threads.groovy
+++ singleBarber_groovy_threads.groovy
@@ -14,39 +14,81 @@ import groovy.transform.Immutable
 def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats ,
                     final Closure hairTrimTime , final Closure nextCustomerWaitTime ) {
   final waitingChairs = new ArrayBlockingQueue<Customer> ( numberOfWaitingSeats )
+  final toShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers )
+  final fromChair = new ArrayBlockingQueue<Customer> ( numberOfCustomers )
+  final fromShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers )
   def customersTurnedAway = 0
   def customersTrimmed = 0
-  final barber = new Runnable ( ) {
+  //  Must subclass Thread here rather than create a Runnable, so that we can execute an interrupt when
+  //  closing the shop.
+  final barber = new Thread ( ) {
     private working = true
-    public void stopWork ( ) { working = false }
+    public void stopWork ( ) {
+      working = false
+      interrupt ( )
+    }
     @Override public void run ( ) {
       while ( working || ( waitingChairs.size ( ) > 0 ) ) {
-        def customer = waitingChairs.take ( )
-        assert customer instanceof Customer
-        println ( "Barber : Starting Customer ${customer.id}." )
-        Thread.sleep ( hairTrimTime ( ) )
-        println ( "Barber : Finished Customer ${customer.id}." )
-        ++customersTrimmed
-        println ( "Shop : Customer ${customer.id} leaving trimmed." )
+        //  Use take here to simulate barber sleeping if there are no customers by blocking.  It means we
+        //  have to use an interrupt when the shop gets closed.
+        try {
+          def customer = waitingChairs.take ( )
+          assert customer instanceof Customer
+          println ( "Barber : Starting Customer ${customer.id}." )
+          Thread.sleep ( hairTrimTime ( ) )
+          println ( "Barber : Finished Customer ${customer.id}." )
+          fromChair.put ( customer )
+        }
+        catch ( InterruptedException ie ) { /* Intentionally left blank. */ }
       }
+      println ( 'Barber : Work over for the day.' )
     }
   }
-  final barberThread = new Thread ( barber )
-  barberThread.start ( )
+  barber.start ( )
+  final shop = new Runnable ( ) {
+    private open = true ;
+    public void closeShop ( ) { open = false }
+    @Override public void run ( ) {
+      while ( open || waitingChairs.size ( ) > 0 ) {
+        def customer = toShop.poll ( )
+        if ( customer ) {
+          if ( waitingChairs.offer ( customer ) ) {
+            println ( "Shop : Customer ${customer.id} takes a seat. ${waitingChairs.size ( )} in use." )
+          }
+          else {
+            ++customersTurnedAway
+            println ( "Shop : Customer ${customer.id} turned away." )
+            fromShop.put ( customer )
+          }
+        }
+        customer = fromChair.poll ( )
+        if ( customer ) {
+          ++customersTrimmed
+          println ( "Shop : Customer ${customer.id} leaving trimmed." )
+          fromShop.put ( customer )
+        }
+      }
+      println ( 'Shop : Closing.' )
+    }
+  }
+  final shopThread = new Thread ( shop )
+  shopThread.start ( )
   for ( number in 0 ..< numberOfCustomers ) {
     Thread.sleep ( nextCustomerWaitTime ( ) )
     println ( "World : Customer ${number} enters the shop." )
-    final customer = new Customer ( number )
-    if ( waitingChairs.offer ( customer ) ) {
-      println ( "Shop : Customer ${customer.id} takes a seat. ${waitingChairs.size ( )} in use." )
-    }
-    else {
-      ++customersTurnedAway
-      println ( "Shop : Customer ${customer.id} turned away." )
-    }
+    toShop.put ( new Customer ( number ) )
   }
+  for ( number in 0 ..< numberOfCustomers ) {
+    def customer = fromShop.take ( )
+    println ( "World : Customer ${customer.id} exits the shop." )
+  }
+  println ( 'World : Time to close up.' )
+  //  If we don't get here then Sweeney Todd is the barber — we have not got as many live customers back as
+  //  we put in.
   barber.stopWork ( )
-  barberThread.join ( )
+  barber.join ( )
+  shop.closeShop ( )
+  shopThread.join ( )
   println ( "\nTrimmed ${customersTrimmed} and turned away ${customersTurnedAway} today." )
 }
 
