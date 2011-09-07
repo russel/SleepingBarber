076f9db6f00b9a8bfb664ee09fa6ab55fb1ed8a0
Russel Winder <russel@russel.org.uk>
2011-09-07 11:46:03 +0100

    Use the latest GPars.

--- singleBarber_groovy_gpars_dataflow.groovy
+++ singleBarber_groovy_gpars_dataflow.groovy
@@ -5,7 +5,8 @@
 //
 //  Copyright © 2010–2011 Russel Winder
 
-@Grab ( 'org.codehaus.gpars:gpars:0.12' )
+//@Grab ( 'org.codehaus.gpars:gpars:0.12' )
+@Grab ( 'org.codehaus.gpars:gpars:1.0-SNAPSHOT' )
 
 import groovy.transform.Immutable
 
