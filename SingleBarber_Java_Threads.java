984b81f9f822c334e72a2c278569c39ad0423127
Russel Winder <russel@russel.org.uk>
2011-09-07 19:14:40 +0100

    Amend the Java threads version to include a world thread.

--- SingleBarber_Java_Threads.java
+++ SingleBarber_Java_Threads.java
@@ -5,12 +5,22 @@
 
 //  Use the default package.
 
+//  The barber is modeled with a thread, with the sleeping being modeled by a blocking read on a queue.  The
+//  queue represents the cutting chairs and the waiting chairs.  Customers are injected into the shop by the
+//  main thread via a queue.  Having either been to the barber and back again, or rejected due to no space
+//  in the queue representing the chairs, the customers is released into the world.  Each thread keeps a
+//  count of the number of customers of each sort.
+
 import java.util.concurrent.ArrayBlockingQueue ;
 
 public class SingleBarber_Java_Threads {
   private final static class Customer {
     public final int id ;
     public Customer ( final int id ) { this.id = id ; }
+  }
+  private final static class SuccessfulCustomer {
+    public final Customer customer ;
+    public SuccessfulCustomer ( final Customer customer ) { this.customer = customer ; }
   }
   private final static class RandomCallingFunction {
     private final int scale ;
@@ -23,10 +33,8 @@ public class SingleBarber_Java_Threads {
   }
   private ArrayBlockingQueue<Customer> waitingChairs ;
   private ArrayBlockingQueue<Customer> toShop ;
-  private ArrayBlockingQueue<Customer> fromChair ;
-  private ArrayBlockingQueue<Customer> fromShop ;
-  private int customersTurnedAway ;
-  private int customersTrimmed ;
+  private ArrayBlockingQueue<SuccessfulCustomer> fromChair ;
+  private ArrayBlockingQueue<Object> fromShop ;
   //  Must subclass thread here rather than create a Runnable, so that we can execute an interrupt when
   //  closing the shop.
   private final class Barber extends Thread {
@@ -38,6 +46,7 @@ public class SingleBarber_Java_Threads {
       interrupt ( ) ;
     }
     @Override public void run ( ) {
+      int customersTrimmed = 0 ;
       while ( working || ( waitingChairs.size ( ) > 0 ) ) {
         try {
           //  Use take here to simulate barber sleeping if there are no customers by blocking.  It means we
@@ -45,22 +54,25 @@ public class SingleBarber_Java_Threads {
           final Customer customer = waitingChairs.take ( ) ;
           System.out.println ( "Barber : Starting Customer " + customer.id + "." ) ;
           try { Thread.sleep ( hairTrimTime.call ( ) ) ; }
-          catch ( InterruptedException iiee ) { throw new RuntimeException ( iiee ) ; }
+          catch ( InterruptedException iiee ) { /* Intentionally left blank. */ }
+          ++customersTrimmed ;
           System.out.println ( "Barber : Finished Customer " + customer.id + "." ) ;
-          try { fromChair.put ( customer ) ; }
+          try { fromChair.put ( new SuccessfulCustomer ( customer ) ) ; }
           catch ( InterruptedException iiee ) { throw new RuntimeException ( iiee ) ; }
         }
         catch ( InterruptedException ie ) { /* Intentionally left blank */ }
       }
-      System.out.println ( "Barber : Work over for the day." ) ;
+      System.out.println ( "Barber : Work over for the day, trimmed " + customersTrimmed + " customers." ) ;
     }
   }
   private final class Shop implements Runnable {
     private boolean open = true ;
     public void closeShop ( ) { open = false ; }
     @Override public void run ( ) {
-      while ( open || waitingChairs.size ( ) > 0 ) {
-        Customer customer = toShop.poll ( ) ;
+      int customersTurnedAway = 0 ;
+      int customersTrimmed = 0 ;
+      while ( open ) {
+        final Customer customer = toShop.poll ( ) ;
         if ( customer != null ) {
           if ( waitingChairs.offer ( customer ) ) {
             System.out.println ( "Shop : Customer " + customer.id + " takes a seat. " + waitingChairs.size ( ) + " in use." ) ;
@@ -72,47 +84,71 @@ public class SingleBarber_Java_Threads {
             catch ( InterruptedException ie ) {  throw new RuntimeException ( ie ) ; }
           }
         }
-        customer = fromChair.poll ( ) ;
-        if ( customer != null ) {
+        final SuccessfulCustomer successfulCustomer = fromChair.poll ( ) ;
+        if ( successfulCustomer != null ) {
           ++customersTrimmed ;
-          System.out.println ( "Shop : Customer " + customer.id + " leaving trimmed." ) ;
-          try { fromShop.put ( customer ) ; }
+          System.out.println ( "Shop : Customer " + successfulCustomer.customer.id + " leaving trimmed." ) ;
+          try { fromShop.put ( successfulCustomer ) ; }
           catch ( InterruptedException ie ) {  throw new RuntimeException ( ie ) ; }
         }
       }
-      System.out.println ( "Shop : Closing." ) ;
+      System.out.println ( "Shop : Closing — Trimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " customers." ) ;
     }
   }
+  private final class World extends Thread {
+    private boolean existing = true ;
+    public void destroyWorld ( ) {
+      existing = false ;
+      interrupt ( ) ;
+    }
+    private final void message ( final int id ) { System.out.println ( "World : Customer " + id + " exits the shop." ) ; }
+    @Override public void run ( ) {
+      int customersTurnedAway = 0 ;
+      int customersTrimmed = 0 ;
+      while ( existing ) {
+        try {
+          final Object customer = fromShop.take ( ) ;
+          if ( customer instanceof Customer ) {
+            ++customersTurnedAway ;
+            message ( ( (Customer) customer ).id ) ;
+          }
+          else if ( customer instanceof SuccessfulCustomer ) {
+            ++customersTrimmed ;
+            message ( ( (SuccessfulCustomer) customer ).customer.id ) ;
+          }
+        }
+        catch ( InterruptedException ie ) { /*Intentionally left blank. */ }
+      }
+      System.out.println ( "\nTrimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " today." ) ;
+      System.out.println ( "World : Non-existent." ) ;
+    }
+  }
+  
   private void runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats ,
-                    final RandomCallingFunction hairTrimTime , final RandomCallingFunction nextCustomerWaitTime ) throws InterruptedException {
+                    final RandomCallingFunction hairTrimTime , final RandomCallingFunction nextCustomerWaitTime )
+    throws InterruptedException {
     waitingChairs = new ArrayBlockingQueue<Customer> ( numberOfWaitingSeats ) ;
     toShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
-    fromChair = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
-    fromShop = new ArrayBlockingQueue<Customer> ( numberOfCustomers ) ;
-    customersTurnedAway = 0 ;
-    customersTrimmed = 0 ;
+    fromChair = new ArrayBlockingQueue<SuccessfulCustomer> ( numberOfCustomers ) ;
+    fromShop = new ArrayBlockingQueue<Object> ( numberOfCustomers ) ;
     final Barber barber = new Barber ( hairTrimTime ) ;
-    barber.start ( ) ;
     final Shop shop = new Shop ( ) ;
-    final Thread shopThread = new Thread (shop ) ;
+    final Thread shopThread = new Thread ( shop ) ;
+    final World world = new World ( ) ;
+    barber.start ( ) ;
     shopThread.start ( ) ;
+    world.start ( ) ;
     for ( int number = 0 ; number < numberOfCustomers ; ++number ) {
       Thread.sleep ( nextCustomerWaitTime.call ( ) ) ;
       System.out.println ( "World : Customer " + number + " enters the shop." ) ;
       toShop.put ( new Customer ( number ) ) ;
     }
-    for ( int number = 0 ; number < numberOfCustomers ; ++number ) {
-      final Customer customer = fromShop.take ( ) ;
-      System.out.println ( "World : Customer " + customer.id + " exits the shop." ) ;
-    }
-    System.out.println ( "World : Time to close up." ) ;
-    //  If we don't get here then Sweeney Todd is the barber — we have not got as many live customers back
-    //  as we put in.
     barber.stopWork ( ) ;
     barber.join ( ) ;
     shop.closeShop ( ) ;
     shopThread.join ( ) ;
-    System.out.println ( "\nTrimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " today." ) ;
+    world.destroyWorld ( ) ;
+    world.join ( ) ;
   }
   public static void main ( final String[] args ) throws InterruptedException {
     ( new SingleBarber_Java_Threads ( ) ).runSimulation ( 20 , 4 ,
