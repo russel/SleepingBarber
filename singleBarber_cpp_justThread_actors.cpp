1d8f92969b92bee8877ac99472de3d2adbd4ea94
Russel Winder <russel@russel.org.uk>
2011-09-22 16:24:38 +0100

    Switch to using a termination message rather than counting people in and out, as per other implementations.

--- singleBarber_cpp_justThread_actors.cpp
+++ singleBarber_cpp_justThread_actors.cpp
@@ -37,19 +37,22 @@ extern jss::actor shop ;
 jss::actor barber (
                    [ ] ( ) {
                      bool running = true ;
+                     unsigned customersTrimmed = 0 ;
                      while ( running ) {
                        jss::actor::receive ( )
                         .match<customer> (
-                                          [ ] ( customer c ) {
+                                          [ & ] ( customer c ) {
                                             std::cout << "Barber : Starting Customer " << c.id << std::endl ;
                                             std::this_thread::sleep_for ( hairTrimTime ( ) ) ;
+                                            ++customersTrimmed ;
                                             std::cout << "Barber : Finished Customer " << c.id << std::endl ;
                                             shop.send ( successfulCustomer ( c ) ) ;
                                           } )
                        .match<std::string> (
                                             [ & ] ( std::string s ) {
-                                              std::cout << "Barber : Knocking off time." << std::endl ;
-                                              shop.send ( std::string ( "Barber" ) ) ;
+                                              //  How to assert s == "closing" ?
+                                              std::cout << "Barber : Knocking off time, " << customersTrimmed << " customers trimmed." << std::endl ;
+                                              shop.send ( std::string ( "clockedOff" ) ) ;
                                               running = false ;
                                             }
                                             ) ;
@@ -58,12 +61,15 @@ jss::actor barber (
 jss::actor shop (
                  [ ] ( ) {
                    unsigned seatsFilled = 0 ;
+                   unsigned customersTrimmed = 0 ;
+                   unsigned customersTurnedAway = 0 ;
                    bool running = true ;
                    while ( running ) {
                      jss::actor::receive ( )
                       .match<customer> (
                                         [ & ] ( customer c ) {
                                           if ( seatsFilled >= numberOfWaitingSeats ) {
+                                            ++customersTurnedAway ;
                                             std::cout << "Shop : Customer " << c.id << " turned away." << std::endl ;
                                             world.send ( c ) ;
                                           }
@@ -76,40 +82,55 @@ jss::actor shop (
                       .match<successfulCustomer> (
                                                   [ & ] ( successfulCustomer s ) {
                                                     --seatsFilled ;
+                                                    ++customersTrimmed ;
                                                     std::cout << "Shop : Customer " << s.c.id << " leaving trimmed." << std::endl ;
                                                     world.send ( s ) ;
                                                   } )
                       .match<std::string> (
                                            [ & ] ( std::string s ) {
-                                             if ( s == std::string ( "World" ) ) {
-                                               std::cout << "Shop : Closing, no more new customers." << std::endl ;
-                                               barber.send ( std::string ( "" ) ) ;
+                                             if ( s == std::string ( "closing" ) ) {
+                                               barber.send ( std::string ( "closing" ) ) ;
                                              }
-                                             else if ( s == std::string ( "Barber" ) ) { running = false ; }
+                                             else if ( s == std::string ( "clockedOff" ) ) {
+                                               std::cout << "Shop : Closing, " <<  customersTrimmed << " trimmed and " << customersTurnedAway << " turned away." << std::endl ;
+                                               world.send ( std::string ( "closed" ) ) ;
+                                               running = false ;
+                                             }
                                            } ) ; 
                    }
                  } ) ;
 
 jss::actor world (
                   [ ] ( ) {
-                    for ( unsigned i = 0 ; i < numberOfCustomers ; ++i ) {
-                      std::this_thread::sleep_for ( customerDelayTime ( ) ) ;
-                      std::cout << "World : Customer " << i << " enters the shop." << std::endl ;
-                      shop.send ( customer ( i ) ) ;
-                    }
-                    std::cout << "World : Time to close the shop." << std::endl ;
-                    shop.send ( std::string ( "World" ) ) ;
                     unsigned customersTurnedAway = 0 ;
                     unsigned customersTrimmed = 0 ;
-                    while ( customersTrimmed + customersTurnedAway < numberOfCustomers ) {
+                    bool running = true ;
+                    while ( running ) {
                       jss::actor::receive ( )
-                       .match<customer> ( [ & ] ( customer c ) { ++customersTurnedAway ; } )
-                       .match<successfulCustomer> ( [ & ] ( successfulCustomer s ) { ++customersTrimmed ; } ) ;
+                       .match<customer> ( [ & ] ( customer c ) {
+                           ++customersTurnedAway ;
+                           std::cout << "World : Customer " << c.id << " exits the shop." << std::endl ;
+                         } )
+                       .match<successfulCustomer> ( [ & ] ( successfulCustomer s ) {
+                           ++customersTrimmed ;
+                           std::cout << "World : Customer " << s.c.id << " exits the shop." << std::endl ;
+                         } )
+                       .match<std::string> ( [ & ] ( std::string s ) {
+                           // How to assert s == "closed" ?
+                           running = false ;
+                           std::cout << "\nTrimmed " << customersTrimmed << " and turned away " << customersTurnedAway << " today." << std::endl ;
+                         } ) ;
                     }
-                    std::cout << "\nTrimmed " << customersTrimmed << " and turned away " << customersTurnedAway << " today." << std::endl ;
                   } ) ;
                     
 int main ( ) {
+  for ( unsigned i = 0 ; i < numberOfCustomers ; ++i ) {
+    std::this_thread::sleep_for ( customerDelayTime ( ) ) ;
+    std::cout << "World : Customer " << i << " enters the shop." << std::endl ;
+    shop.send ( customer ( i ) ) ;
+  }
+  std::cout << "World : No more customers, time to close the shop." << std::endl ;
+  shop.send ( std::string ( "closing" ) ) ;
   return 0 ;
 }
                       
