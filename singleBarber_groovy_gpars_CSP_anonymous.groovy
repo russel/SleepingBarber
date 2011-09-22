c592777a6acfd55e81158cd2f743f334386f9f6f
Russel Winder <russel@russel.org.uk>
2011-09-22 22:56:09 +0100

    Rework into the more standard design using case classes.

--- singleBarber_groovy_gpars_CSP_anonymous.groovy
+++ singleBarber_groovy_gpars_CSP_anonymous.groovy
@@ -25,25 +25,31 @@ import groovyx.gpars.csp.PAR
 import groovyx.gpars.csp.ALT
 
 @Immutable class Customer { Integer id }
+@Immutable class SuccessfulCustomer { Customer c }
 
 def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats ,
                     final Closure hairTrimTime , final Closure nextCustomerWaitTime ) {
   final worldToShopChannel = Channel.one2one ( )
   final shopToBarberChannel = Channel.one2one ( new Buffer ( numberOfWaitingSeats ) )
   final barberToShopChannel = Channel.one2one ( )
+  final shopToWorldChannel = Channel.one2one ( )
   final barber = new CSProcess ( ) {
     @Override public void run ( ) {
       final fromShopChannel = shopToBarberChannel.in ( )
       final toShopChannel = barberToShopChannel.out ( )
+      def customersTrimmed = 0
       while ( true ) {
         final customer = fromShopChannel.read ( )
         if ( customer == '' ) { break }
         assert customer instanceof Customer
-        println ( "Barber : Starting Customer ${customer.id}." )
+        println "Barber : Starting Customer ${customer.id}."
         Thread.sleep ( hairTrimTime ( ) )
-        println ( "Barber : Finished Customer ${customer.id}." )
-        toShopChannel.write ( customer )
+        ++customersTrimmed
+        println "Barber : Finished Customer ${customer.id}."
+        toShopChannel.write ( new SuccessfulCustomer ( customer ) )
       }
+      println "Barber : Work finished for today, trimmed ${customersTrimmed}."
+      toShopChannel.write ( '' )
     }
   }
   final shop = new CSProcess ( ) {
@@ -51,29 +57,32 @@ def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats
       final fromBarberChannel = barberToShopChannel.in ( )
       final fromWorldChannel = worldToShopChannel.in ( )
       final toBarberChannel = shopToBarberChannel.out ( )
+      final toWorldChannel = shopToWorldChannel.out ( )
       final selector = new ALT ( [ fromBarberChannel , fromWorldChannel ] )
       def seatsTaken = 0
       def customersTurnedAway = 0
       def customersTrimmed = 0
       def isOpen = true
-    mainloop: 
-      while ( true ) {
+      while ( isOpen ) {
         switch ( selector.select ( ) ) { 
          case 0 : //////// From the Barber ////////
            def customer = fromBarberChannel.read ( )
-           assert customer instanceof Customer
-           --seatsTaken
-           ++customersTrimmed
-           println ( "Shop : Customer ${customer.id} leaving trimmed." )
-           if ( ! isOpen && ( seatsTaken == 0 ) ) {
-             println ( "\nTrimmed ${customersTrimmed} and turned away ${customersTurnedAway} today." )
-             toBarberChannel.write ( '' )
-             break mainloop
+           if ( customer == '' ) {
+             println ( "Shop : Closing --- ${customersTrimmed} trimmed and ${customersTurnedAway} turned away." )
+             isOpen = false
+             toWorldChannel.write ( customer )
+           }
+           else {
+             assert customer instanceof SuccessfulCustomer
+             --seatsTaken
+             ++customersTrimmed
+             println ( "Shop : Customer ${customer.c.id} leaving trimmed." )
+             toWorldChannel.write ( customer )
            }
            break
          case 1 : //////// From the World ////////
            def customer = fromWorldChannel.read ( )
-           if ( customer == '' ) { isOpen = false }
+           if ( customer == '' ) { toBarberChannel.write ( customer ) }
            else {
              assert customer instanceof Customer
              if ( seatsTaken < numberOfWaitingSeats ) {
@@ -84,6 +93,7 @@ def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats
              else {
                println ( "Shop : Customer ${customer.id} turned away." )
                ++customersTurnedAway
+               toWorldChannel.write ( customer )
              }
            }
            break
@@ -93,9 +103,30 @@ def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats
       }
     }
   }
-  final world = new CSProcess ( ) {
+  final worldSink = new CSProcess ( ) {
     @Override public void run ( ) {
-      def toShopChannel = worldToShopChannel.out ( )
+      final fromShopChannel = shopToWorldChannel.in ( )
+      def customersTurnedAway = 0
+      def customersTrimmed = 0
+      while ( true ) {
+        final customer = fromShopChannel.read ( )
+        if ( customer instanceof Customer ) {
+          ++customersTurnedAway
+        }
+        else if ( customer instanceof SuccessfulCustomer ) {
+          ++customersTrimmed
+        }
+        else if ( customer == '' ) {
+          break
+        }
+        else { throw new RuntimeException ( "World got a message of an unexpected type ${customer.class}." ) }
+      }
+      println "\nTrimmed ${customersTrimmed} and turned away ${customersTurnedAway} today."
+    }
+  }
+  final worldSource = new CSProcess ( ) {
+    @Override public void run ( ) {
+      final toShopChannel = worldToShopChannel.out ( )
       for ( number in 0 ..< numberOfCustomers ) {
         Thread.sleep ( nextCustomerWaitTime ( ) )
         println ( "World : Customer ${number} enters the shop." )
@@ -104,7 +135,7 @@ def runSimulation ( final int numberOfCustomers , final int numberOfWaitingSeats
       toShopChannel.write ( '' )
     }
   }
-  new PAR ( [ barber , shop , world ] ).run ( )
+  new PAR ( [ barber , shop , worldSink , worldSource ] ).run ( )
 }
 
 runSimulation ( 20 , 4 , { ( Math.random ( ) * 60 + 10 ) as int }, { ( Math.random ( ) * 20 + 10 ) as int } )
