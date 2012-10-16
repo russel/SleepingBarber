71e1dac2becb7219f3878af1369f03e69423e9f8
Russel Winder <russel@winder.org.uk>
2012-10-16 09:18:25 +0100

    Update the formatting of the Scala version.

--- SingleBarber_Scala_Actors.scala
+++ SingleBarber_Scala_Actors.scala
@@ -14,98 +14,97 @@ import scala.actors.Actor._
 import scala.util.Random
 
 object SingleBarber_Scala_Actors extends App {
-  case class Customer ( id : Int )
-  case class SuccessfulCustomer ( customer : Customer )
+  case class Customer(id:Int)
+  case class SuccessfulCustomer(customer:Customer)
   case object CloseShop
   case object BarberStoppedWork
-  def runSimulation ( numberOfCustomers : Int , numberOfWaitingSeats : Int , hairTrimTime : ( ) => Int , nextCustomerWaitingTime : ( ) => Int ) {
+  def runSimulation(numberOfCustomers:Int , numberOfWaitingSeats:Int , hairTrimTime:() => Int , nextCustomerWaitingTime:() => Int) {
     //  Due to the cross-references of the various actor objects, the type inference system cannot resolve
     //  the types appropriately, and so the types of some of the actors must be specified, so declare the
     //  types of all of them. Moreover, the definitions have to be lazy so as to deal with the order of use
     //  versus definition.
-    lazy val barber : Actor = Actor.actor {
+    lazy val barber:Actor = Actor.actor {
       var customersTrimmed = 0
       var working = true
-      loopWhile ( working ) {
+      loopWhile (working) {
         react {
-          case customer : Customer =>
-            println ( "Barber : Starting Customer " + customer.id )
-            Thread.sleep ( hairTrimTime ( ) )
-            println ( "Barber : Finished Customer " + customer.id )
+          case customer:Customer =>
+            println("Barber: Starting Customer " + customer.id)
+            Thread.sleep(hairTrimTime())
+            println("Barber: Finished Customer " + customer.id)
             customersTrimmed += 1
-            shop ! new SuccessfulCustomer ( customer )
+            shop ! new SuccessfulCustomer(customer)
           case CloseShop =>
-            println ( "Barber : Work over for the day, trimmed " + customersTrimmed + "." )
+            println("Barber: Work over for the day, trimmed " + customersTrimmed + ".")
             shop ! BarberStoppedWork
             working = false
         }
       }
     }
-    lazy val shop : Actor = Actor.actor {
+    lazy val shop:Actor = Actor.actor {
       var seatsFilled = 0
       var customersTrimmed = 0
       var customersTurnedAway = 0
       var open = true
-      loopWhile ( open ) {
+      loopWhile (open) {
         react {
-          case customer : Customer =>
-            if ( seatsFilled <= numberOfWaitingSeats ) {
+          case customer:Customer =>
+            if(seatsFilled <= numberOfWaitingSeats) {
               seatsFilled += 1
-              println ( "Shop : Customer " + customer.id + " takes a seat. " + seatsFilled + " in use." )
+              println("Shop: Customer " + customer.id + " takes a seat. " + seatsFilled + " in use.")
               barber ! customer
             } else {
               customersTurnedAway += 1
-              println ( "Shop : Customer " + customer.id + " turned away." )
+              println("Shop: Customer " + customer.id + " turned away.")
               world ! customer
             }
-          case successfulCustomer : SuccessfulCustomer =>
+          case successfulCustomer:SuccessfulCustomer =>
             seatsFilled -= 1
             customersTrimmed += 1
-            println ( "Shop : Customer " + successfulCustomer.customer.id + " leaving trimmed." )
+            println("Shop: Customer " + successfulCustomer.customer.id + " leaving trimmed.")
             world ! successfulCustomer
           case CloseShop =>
             barber ! CloseShop
           case BarberStoppedWork =>
-            println ( "Shop : Closing — " + customersTrimmed + " trimmed and " + customersTurnedAway + " turned away." )
+            println("Shop: Closing — " + customersTrimmed + " trimmed and " + customersTurnedAway + " turned away.")
             open = false
             world ! CloseShop
         }
       }
     }
-    lazy val world : Actor = Actor.actor {
+    lazy val world:Actor = Actor.actor {
       //  We know that the Vogon constructor fleet is coming to destroy the world to make way for an
       //  hyperspace by-pass.
       var notVogoned = true
       var customersTurnedAway = 0
       var customersTrimmed = 0
-      while ( notVogoned ) {
+      while (notVogoned) {
         var id = 0
-        def message ( id : Int ) = { println ( "World : Customer " + id + " exits the shop." ) }
+        def message(id:Int) = { println("World: Customer " + id + " exits the shop.") }
         receive {
-          case customer : Customer =>
+          case customer:Customer =>
             customersTurnedAway += 1
-            message ( customer.id )
-          case successfulCustomer : SuccessfulCustomer =>
+            message(customer.id)
+          case successfulCustomer:SuccessfulCustomer =>
             customersTrimmed += 1
-            message ( successfulCustomer.customer.id )
+            message(successfulCustomer.customer.id)
           case CloseShop =>
-            println ( "\nTrimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " today." )
+            println("\nTrimmed " + customersTrimmed + " and turned away " + customersTurnedAway + " today.")
             notVogoned = false
         }
       }
-      println ( "World : The Vogons have arrived. Time to explode." )
+      println("World: The Vogons have arrived. Time to explode.")
     }
-    barber.start ( )
-    shop.start ( )
-    world.start ( )
-    for ( number <- 0 to numberOfCustomers ) {
-      Thread.sleep ( nextCustomerWaitingTime ( ) )
-      println ( "World : Customer " + number + " enters the shop." )
-      shop ! new Customer ( number )
+    barber.start()
+    shop.start()
+    world.start()
+    for(number <- 0 to numberOfCustomers) {
+      Thread.sleep(nextCustomerWaitingTime())
+      println("World: Customer " + number + " enters the shop.")
+      shop ! new Customer(number)
     }
     shop ! CloseShop
   }
-  val r = new Random ( )
-  runSimulation ( 20 , 4 , ( ) => r.nextInt ( 6 ) + 1 , ( ) => r.nextInt ( 2 ) + 1 )
+  val r = new Random()
+  runSimulation(20 , 4 ,() => r.nextInt(6) + 1 ,() => r.nextInt(2) + 1)
 }
-  
